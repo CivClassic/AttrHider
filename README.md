16c6f16e3f047f1562fd6bc2267ae05d6dd02d4e
Alexander <protonull@protonmail.com>
2020-12-11 17:28:15 +0000

    Update

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 AttrHider
 =========
 
-Stops Bukkit Minecraft server from sending enchantment, item damage and potion duration data. 
+Stops Bukkit Minecraft server from sending enchantment, item damage and potion duration data.
 
 Updated for Spigot 1.12.2 as of May 13th, 2019 thanks to Okx (https://github.com/okx-code/MetaHider)
 
 ## Description
 
-Minecraft servers send packets containing data about other players to Minecraft clients. Some of this data is not necessarily needed by the client, such as potion durations, equipment enchantment types, and health. The vanilla Minecraft client does not display these for other entities other than the player themself, but hacked clients can. This gives an edge to players with hacked clients over those who use the default client, especially in PvP situations. 
+Minecraft servers send packets containing data about other players to Minecraft clients. Some of this data is not
+necessarily needed by the client, such as potion durations, equipment enchantment types, and health. The vanilla
+Minecraft client does not display these for other entities other than the player themself, but hacked clients can. This
+gives an edge to players with hacked clients over those who use the default client, especially in PvP situations.
 
-AttrHider solves this by modifying outgoing packets from the server that concern potion durations, equipment enchantments, and entity health. Data that clients do not need to know about are replaced with fake values. Players using default clients will not see any differences, but players using hacked clients will see false information.
+AttrHider solves this by modifying outgoing packets from the server that concern potion durations, equipment
+enchantments, and entity health. Data that clients do not need to know about are replaced with fake values. Players
+using default clients will not see any differences, but players using hacked clients will see false information.
--- src/main/java/co/civcraft/attrhider/AttrHider.java
+++ src/main/java/co/civcraft/attrhider/AttrHider.java
@@ -8,6 +8,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import java.util.List;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -23,184 +24,184 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionData;
 
-import java.util.List;
-
 public class AttrHider extends JavaPlugin {
-  @Override
-  public void onEnable() {
-    saveDefaultConfig();
 
-    ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
 
-    if (getConfig().getBoolean("hide-item-meta")) {
-      manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_EQUIPMENT) {
-        @Override
-        public void onPacketSending(PacketEvent event) {
-          PacketContainer packet = event.getPacket();
-          if (event.getPlayer().hasPermission("attrhider.bypass")) {
-            return;
-          }
+		ProtocolManager manager = ProtocolLibrary.getProtocolManager();
 
-          ItemStack item = packet.getItemModifier().read(0);
-          if (item == null) {
-            return;
-          }
-          if (!shouldBeObfuscated(item.getType())) {
-            // remove some meta information for all items
-            ItemMeta meta = item.getItemMeta();
-            if(meta != null) {
-	            meta.setLore(null);
-	            meta.setDisplayName(null);
-	            meta.setUnbreakable(false);
-	            item.setItemMeta(meta);
-            }
-            packet.getItemModifier().write(0, item);
-            return;
-          }
+		if (getConfig().getBoolean("hide-item-meta")) {
+			manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_EQUIPMENT) {
+				@Override
+				public void onPacketSending(PacketEvent event) {
+					PacketContainer packet = event.getPacket();
+					if (event.getPlayer().hasPermission("attrhider.bypass")) {
+						return;
+					}
 
-          // more thorough obfuscation
-          Color colour = null;
-          ItemMeta meta = item.getItemMeta();
-          if (meta instanceof LeatherArmorMeta) {
-            LeatherArmorMeta lam = (LeatherArmorMeta) item.getItemMeta();
-            colour = lam.getColor();
-          }
-          ItemStack newItem = new ItemStack(item.getType(), 1);
-          if (meta.hasEnchants()) {
-            newItem.addEnchantment(Enchantment.DURABILITY, 1);
-          }
-          if (colour != null) {
-            LeatherArmorMeta lam = (LeatherArmorMeta) item.getItemMeta();
-            lam.setColor(colour);
-            newItem.setItemMeta(lam);
-          }
-          packet.getItemModifier().write(0, newItem);
-        }
-      });
-    }
-    if (getConfig().getBoolean("hide-potion-meta")) {
-      manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_EQUIPMENT) {
-        @Override
-        public void onPacketSending(PacketEvent event) {
-          PacketContainer packet = event.getPacket();
-          ItemStack item = packet.getItemModifier().read(0);
+					ItemStack item = packet.getItemModifier().read(0);
+					if (item == null) {
+						return;
+					}
+					if (!shouldBeObfuscated(item.getType())) {
+						// remove some meta information for all items
+						ItemMeta meta = item.getItemMeta();
+						if (meta != null) {
+							meta.setLore(null);
+							meta.setDisplayName(null);
+							meta.setUnbreakable(false);
+							item.setItemMeta(meta);
+						}
+						packet.getItemModifier().write(0, item);
+						return;
+					}
 
-          if (item == null
-              || !isPotion(item.getType())
-              || event.getPlayer().hasPermission("attrhider.bypass")) {
-            return;
-          }
+					// more thorough obfuscation
+					Color colour = null;
+					ItemMeta meta = item.getItemMeta();
+					if (meta instanceof LeatherArmorMeta) {
+						LeatherArmorMeta lam = (LeatherArmorMeta) item.getItemMeta();
+						colour = lam.getColor();
+					}
+					ItemStack newItem = new ItemStack(item.getType(), 1);
+					if (meta.hasEnchants()) {
+						newItem.addEnchantment(Enchantment.DURABILITY, 1);
+					}
+					if (colour != null) {
+						LeatherArmorMeta lam = (LeatherArmorMeta) item.getItemMeta();
+						lam.setColor(colour);
+						newItem.setItemMeta(lam);
+					}
+					packet.getItemModifier().write(0, newItem);
+				}
+			});
+		}
+		if (getConfig().getBoolean("hide-potion-meta")) {
+			manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_EQUIPMENT) {
+				@Override
+				public void onPacketSending(PacketEvent event) {
+					PacketContainer packet = event.getPacket();
+					ItemStack item = packet.getItemModifier().read(0);
 
-          // only show the potion type
-          PotionMeta meta = (PotionMeta) item.getItemMeta();
-          meta.clearCustomEffects();
-          PotionData base = meta.getBasePotionData();
-          meta.setBasePotionData(new PotionData(base.getType()));
-          item.setItemMeta(meta);
+					if (item == null
+							|| !isPotion(item.getType())
+							|| event.getPlayer().hasPermission("attrhider.bypass")) {
+						return;
+					}
 
-          packet.getItemModifier().write(0, item);
-        }
-      });
-    }
-    if (getConfig().getBoolean("hide-potion-effects")) {
-      manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_EFFECT) {
-        @Override
-        public void onPacketSending(PacketEvent event) {
-          PacketContainer packet = event.getPacket();
-          if (event.getPlayer().hasPermission("attrhider.bypass")) {
-            return;
-          }
-          StructureModifier<Integer> ints = packet.getIntegers();
-          if (event.getPlayer().getEntityId() == ints.read(0)) {
-            return;
-          }
-          // set amplifier to 0
-          packet.getBytes().write(1, (byte) 0);
-          // set duration to 0
-          ints.write(1, 0);
-        }
-      });
-    }
-    if (getConfig().getBoolean("hide-health")) {
-      manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_METADATA) {
-        @Override
-        public void onPacketSending(PacketEvent event) {
-          PacketContainer packet = event.getPacket();
-          Player player = event.getPlayer();
-          if (player.hasPermission("attrhider.bypass")) {
-            return;
-          }
-          Entity entity = event.getPacket().getEntityModifier(event).read(0);
-          StructureModifier<List<WrappedWatchableObject>> modifier = packet.getWatchableCollectionModifier();
-          List<WrappedWatchableObject> read = modifier.read(0);
+					// only show the potion type
+					PotionMeta meta = (PotionMeta) item.getItemMeta();
+					meta.clearCustomEffects();
+					PotionData base = meta.getBasePotionData();
+					meta.setBasePotionData(new PotionData(base.getType()));
+					item.setItemMeta(meta);
 
-          if (entity == null || player.getUniqueId().equals(entity.getUniqueId())
-            	|| entity.getEntityId() == player.getEntityId()
-              || !(entity instanceof LivingEntity)
-              || entity instanceof EnderDragon
-              || entity instanceof Wither
-              || entity.getPassengers().contains(player)) {
-            return;
-          }
+					packet.getItemModifier().write(0, item);
+				}
+			});
+		}
+		if (getConfig().getBoolean("hide-potion-effects")) {
+			manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_EFFECT) {
+				@Override
+				public void onPacketSending(PacketEvent event) {
+					PacketContainer packet = event.getPacket();
+					if (event.getPlayer().hasPermission("attrhider.bypass")) {
+						return;
+					}
+					StructureModifier<Integer> ints = packet.getIntegers();
+					if (event.getPlayer().getEntityId() == ints.read(0)) {
+						return;
+					}
+					// set amplifier to 0
+					packet.getBytes().write(1, (byte) 0);
+					// set duration to 0
+					ints.write(1, 0);
+				}
+			});
+		}
+		if (getConfig().getBoolean("hide-health")) {
+			manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.ENTITY_METADATA) {
+				@Override
+				public void onPacketSending(PacketEvent event) {
+					PacketContainer packet = event.getPacket();
+					Player player = event.getPlayer();
+					if (player.hasPermission("attrhider.bypass")) {
+						return;
+					}
+					Entity entity = event.getPacket().getEntityModifier(event).read(0);
+					StructureModifier<List<WrappedWatchableObject>> modifier = packet.getWatchableCollectionModifier();
+					List<WrappedWatchableObject> read = modifier.read(0);
 
-          for (WrappedWatchableObject obj : read) {
-            if (obj.getIndex() == 8) {
-              float value = (float) obj.getValue();
-              if (value > 0) {
-                obj.setValue(1f);
-              }
-            }
-          }
-        }
-      });
-    }
-  }
+					if (entity == null || player.getUniqueId().equals(entity.getUniqueId())
+							|| entity.getEntityId() == player.getEntityId()
+							|| !(entity instanceof LivingEntity)
+							|| entity instanceof EnderDragon
+							|| entity instanceof Wither
+							|| entity.getPassengers().contains(player)) {
+						return;
+					}
 
-  private boolean shouldBeObfuscated(Material type) {
-    return type == Material.DIAMOND_HELMET
-        || type == Material.DIAMOND_CHESTPLATE
-        || type == Material.DIAMOND_LEGGINGS
-        || type == Material.DIAMOND_BOOTS
-        || type == Material.IRON_HELMET
-        || type == Material.IRON_CHESTPLATE
-        || type == Material.IRON_LEGGINGS
-        || type == Material.IRON_BOOTS
-        || type == Material.GOLDEN_HELMET
-        || type == Material.GOLDEN_CHESTPLATE
-        || type == Material.GOLDEN_LEGGINGS
-        || type == Material.GOLDEN_BOOTS
-        || type == Material.LEATHER_HELMET
-        || type == Material.LEATHER_CHESTPLATE
-        || type == Material.LEATHER_LEGGINGS
-        || type == Material.LEATHER_BOOTS
-        || type == Material.DIAMOND_SWORD
-        || type == Material.GOLDEN_SWORD
-        || type == Material.IRON_SWORD
-        || type == Material.STONE_SWORD
-        || type == Material.WOODEN_SWORD
-        || type == Material.DIAMOND_AXE
-        || type == Material.GOLDEN_AXE
-        || type == Material.IRON_AXE
-        || type == Material.STONE_AXE
-        || type == Material.WOODEN_AXE
-        || type == Material.DIAMOND_PICKAXE
-        || type == Material.GOLDEN_PICKAXE
-        || type == Material.IRON_PICKAXE
-        || type == Material.STONE_PICKAXE
-        || type == Material.WOODEN_PICKAXE
-        || type == Material.DIAMOND_SHOVEL
-        || type == Material.GOLDEN_SHOVEL
-        || type == Material.IRON_SHOVEL
-        || type == Material.STONE_SHOVEL
-        || type == Material.WOODEN_SHOVEL
-        || type == Material.FIREWORK_ROCKET
-        || type == Material.WRITTEN_BOOK
-        || type == Material.ENCHANTED_BOOK;
-  }
+					for (WrappedWatchableObject obj : read) {
+						if (obj.getIndex() == 8) {
+							float value = (float) obj.getValue();
+							if (value > 0) {
+								obj.setValue(1f);
+							}
+						}
+					}
+				}
+			});
+		}
+	}
 
-  private boolean isPotion(Material type) {
-    return type == Material.POTION
-        || type == Material.LINGERING_POTION
-        || type == Material.SPLASH_POTION;
-  }
+	private boolean shouldBeObfuscated(Material type) {
+		return type == Material.DIAMOND_HELMET
+				|| type == Material.DIAMOND_CHESTPLATE
+				|| type == Material.DIAMOND_LEGGINGS
+				|| type == Material.DIAMOND_BOOTS
+				|| type == Material.IRON_HELMET
+				|| type == Material.IRON_CHESTPLATE
+				|| type == Material.IRON_LEGGINGS
+				|| type == Material.IRON_BOOTS
+				|| type == Material.GOLDEN_HELMET
+				|| type == Material.GOLDEN_CHESTPLATE
+				|| type == Material.GOLDEN_LEGGINGS
+				|| type == Material.GOLDEN_BOOTS
+				|| type == Material.LEATHER_HELMET
+				|| type == Material.LEATHER_CHESTPLATE
+				|| type == Material.LEATHER_LEGGINGS
+				|| type == Material.LEATHER_BOOTS
+				|| type == Material.DIAMOND_SWORD
+				|| type == Material.GOLDEN_SWORD
+				|| type == Material.IRON_SWORD
+				|| type == Material.STONE_SWORD
+				|| type == Material.WOODEN_SWORD
+				|| type == Material.DIAMOND_AXE
+				|| type == Material.GOLDEN_AXE
+				|| type == Material.IRON_AXE
+				|| type == Material.STONE_AXE
+				|| type == Material.WOODEN_AXE
+				|| type == Material.DIAMOND_PICKAXE
+				|| type == Material.GOLDEN_PICKAXE
+				|| type == Material.IRON_PICKAXE
+				|| type == Material.STONE_PICKAXE
+				|| type == Material.WOODEN_PICKAXE
+				|| type == Material.DIAMOND_SHOVEL
+				|| type == Material.GOLDEN_SHOVEL
+				|| type == Material.IRON_SHOVEL
+				|| type == Material.STONE_SHOVEL
+				|| type == Material.WOODEN_SHOVEL
+				|| type == Material.FIREWORK_ROCKET
+				|| type == Material.WRITTEN_BOOK
+				|| type == Material.ENCHANTED_BOOK;
+	}
+
+	private boolean isPotion(Material type) {
+		return type == Material.POTION
+				|| type == Material.LINGERING_POTION
+				|| type == Material.SPLASH_POTION;
+	}
+
 }
